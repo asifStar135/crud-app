c8b751d32458be7ee557e0e5cb31988d1c02e0c7
AsifStar786 <asifstar135@gmail.com>
2022-11-25 11:23:33 +0530

    make it better

--- app.js
+++ app.js
@@ -21,9 +21,9 @@ cloudinary.config({
 connectDb();
 
 //accessing client/build/index.html
-if (process.env.NODE_ENV !== "production") {
+if (process.env.NODE_ENV === "production") {
     App.use(express.static(path.join(__dirname, "./client/build")));
-  
+
     App.get("*", (req, res) => {
       res.sendFile(path.resolve(__dirname, "./client/build/index.html"));
     });
